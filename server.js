c1dec39989de6dea2da409a97014f79e92a0ec49
annielam0623 <annzhouyy@gmail.com>
2023-04-08 20:30:15 -0700

    add band, index & 1st migrate

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+//SEQUELIZE CONNECTION
+// const sequelize = new Sequelize({
+//     storage: process.env.PG_URI,
+//     dialect: Postgres,
+//     username: 'postgres',
+//     password: process.env.PG_PW
+// })
+
+
+// try {
+//     sequelize.authenticate()
+//     console.log(`Connected with Sequelize at ${process.env.PG_URI}`)
+// } catch(err) {
+//     console.log(`Unable to connect to database`, err)
+// }
 
 // ROOT
 app.get('/', (req, res) => {
